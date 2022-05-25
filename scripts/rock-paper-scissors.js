bb457bd00cebc436a1606dfb5bafff7c859fdd00
Phillip Novicki <Phillip.Novicki@gmail.com>
2022-05-25 13:23:49 -0500

    Playround Function Added

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,5 +1,6 @@
 var player1, player2;
 
+const computerSelection = computerPlay();
 function computerPlay(){
     // pick a random number between 0 and 2
     let choice = Math.floor(Math.random() * 3);
@@ -18,4 +19,30 @@ function computerPlay(){
     return text;
 }
 
-console.log(computerPlay());
+// Write a function that converts both choices to numbers and defines a winner
+function playRound(playerSelection, computerSelection){
+    let p = playerSelection.toLowerCase();
+    let c = computerSelection.toLowerCase();
+    let playerWins;
+
+    switch(p){
+        case 'rock':
+            switch(c){
+                case 'rock':
+                    playerWins = "It's a tie!"
+                    break;
+                case 'paper':
+                    playerWins = "Computer Wins!"
+                    break;
+                case 'scissors':
+                    playerWins = "Player Wins!"
+                    break;
+            }
+    }
+    return playerWins;
+}
+const playerSelection = 'Rock';
+
+
+
+console.log(playRound(playerSelection, computerSelection));
